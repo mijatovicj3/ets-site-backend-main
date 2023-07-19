637293376e167111eae1e5b822fe6c7a383cedc5
mijatovicj3 <98957665+mijatovicj3@users.noreply.github.com>
2023-07-19 20:49:19 +0200

    Update ClassController.php

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -10,17 +10,26 @@ class ClassController extends Controller
 {
     public function index()
     {
-        return ClassModel::all();
+        $classes = ClassModel::all();
+        return  $classes ;
     }
 
     public function create()
     {
-        //
+       return view('classes.create');
     }
 
     public function store(Request $request)
     {
-        //
+         $request->validate([
+            'name' => 'required|string',
+            'year' => 'required|integer',
+        ]);
+
+        ClassModel::create($request->all());
+
+        return redirect()->route('classes.index')->with('success', 'Class created successfully.');
+    
     }
 
     public function show(string $id)
@@ -30,16 +39,28 @@ class ClassController extends Controller
 
     public function edit(string $id)
     {
-        //
+       return view('classes.edit', compact('class'));
     }
 
     public function update(Request $request, string $id)
     {
-        //
+         $request->validate([
+            'name' => 'required|string',
+            'year' => 'required|integer',
+        ]);
+
+        $class->update($request->all());
+
+        return redirect()->route('classes.index')->with('success', 'Class updated successfully.');
+    
     }
 
     public function destroy(string $id)
     {
-        //
+         $class->delete();
+
+        return redirect()->route('classes.index')->with('success', 'Class deleted successfully.');
+
+    
     }
 }
